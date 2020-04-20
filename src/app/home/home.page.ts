2874dfb4abb5ac7327aee77e99fb81e26ddb2c34
alex-steinberg <steinberg.alex@gmail.com>
2020-04-20 11:07:03 +0200

    show how the swiper 'controller' module doesn't work

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  @ViewChild('headerSlider') headerSlider: IonSlides;
+  @ViewChild('contentSlider') contentSlider: IonSlides;
+  contentOpts: any = {
+    controller: {
+      control: this.headerSlider
+    },
+    slidesPerView: 1
+  }
+  headerOpts: any = {
+    slidesPerView: 1
+  }
 
   constructor() {}
 
